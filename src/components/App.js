d8d9b2fced9f35fab6c3f497fd4dd7a8309809ba
tushareshukla <123384228+tushareshukla@users.noreply.github.com>
2024-03-16 17:59:28 +0000

    push

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,18 @@
 import React from "react";
 import './../styles/App.css';
 
+const clickCounter = () =>{
+  const [count, setCounts] = useState(0)
+  const handleClick = () =>{
+    setCounts = count+1
+  }
+}
+
 const App = () => {
   return (
     <div>
+      The button clicked {count} time{count!== 0 && 's'}
+      <button onClick ={handleClick}></button>
         {/* Do not remove the main div */}
     </div>
   )
